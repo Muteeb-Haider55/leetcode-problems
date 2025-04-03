682b4edb1522c1d0f8327f9c3f36d38be85b20d0
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-04-03 20:53:02 +0500

    Time: 0 ms (100.00%) | Memory: 9.6 MB (74.46%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) 
+            return false;
+
+        int count[26] = {0};  // Array to store frequency of characters
+
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;  // Increment for s
+            count[t[i] - 'a']--;  // Decrement for t
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0)  // If any letter count is not balanced
+                return false;
+        }
+
+        return true;
+    }
+};
