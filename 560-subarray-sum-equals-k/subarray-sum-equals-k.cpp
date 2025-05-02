00e43300e43744c0bc40f2feb316a031c198d09f
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-05-02 23:00:09 +0500

    Time: 40 ms (73.17%) | Memory: 45.3 MB (76.29%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-    //Brute Force Approach
-         int count = 0;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            int sum=0;
-            for(int j=i; j<n; j++){
-                sum+=nums[j];
-                if(sum==k) count++;
+        // Brute Force Approach
+        /*  int count = 0;
+         int n = nums.size();
+         for(int i=0; i<n; i++){
+             int sum=0;
+             for(int j=i; j<n; j++){
+                 sum+=nums[j];
+                 if(sum==k) count++;
+             }
+         }
+         return count;*/
+        // Optimized Approach
+        unordered_map<int, int> prefixSumCount;
+        prefixSumCount[0] = 1;
+        int count = 0, sum = 0;
+        for (int num : nums) {
+            sum += num;
+            if (prefixSumCount.find(sum - k) != prefixSumCount.end()) {
+                count += prefixSumCount[sum - k];
             }
+            prefixSumCount[sum]++;
         }
-        return count;
-       
-//Optimized Approach
 
+        return count;
     }
 };
