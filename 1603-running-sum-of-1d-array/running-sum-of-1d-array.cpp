6f9cc25d61c87d3adb4184b0f17acecd2600f3e1
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-18 15:54:33 +0500

    Time: 3 ms (6.03%) | Memory: 12.6 MB (49.98%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        for (int i = 1; i < nums.size(); i++) {
+        // Optimized Aproach
+       
+       for (int i = 1; i < nums.size(); i++) {
             nums[i] += nums[i - 1];
         }
         return nums;
+        
+        // Brute Force Approach
+       /* 
+        vector <int> res = nums;
+        int n = nums.size();
+        for(int i=1; i<n; i++){
+            res[i] += res[i-1];
+        }
+        return res;
+        */ 
     }
 };
