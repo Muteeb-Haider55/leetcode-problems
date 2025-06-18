b1a4540557bd4b181f2ccc4374d907f3042157b3
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-18 16:20:30 +0500

    Time: 0 ms (100%), Space: 10.8 MB (43.22%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int maximum = 0;
+        int alt = 0;
         int maxalt = 0;
         for (int i = 0; i < gain.size(); i++) {
-            maximum += gain[i];
-            maxalt = max(maximum, maxalt);
+            alt += gain[i];
+            maxalt = max(alt, maxalt);
         }
         return maxalt;
     }
