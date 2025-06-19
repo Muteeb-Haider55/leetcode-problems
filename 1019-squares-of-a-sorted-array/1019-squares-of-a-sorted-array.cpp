ed0415a8e3c970dbaf276e6de6224733f29c2690
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-19 12:16:19 +0500

    Time: 0 ms (100%), Space: 30.2 MB (85.44%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        
+
         // Using range-based for loop with reference (&)
         // to modify the original vector in-place.
+        /*
         for(auto &num : nums){
             num *= num;
         }
+        */
 
         // ❌ This below version does NOT work for in-place modification:
         // for(int num : nums){
-        //     num *= num; // 'num' here is a copy of each element — the original 'nums' remains unchanged
+        //     num *= num; // 'num' here is a copy of each element — the
+        //     original 'nums' remains unchanged
         // }
 
         // ✔️ A simple classic for loop could also be used:
         // for(int i = 0; i < nums.size(); i++){
-        //     nums[i] *= nums[i]; // Directly modifies original elements using index access
+        //     nums[i] *= nums[i]; // Directly modifies original elements using
+        //     index access
         // }
 
         // Sorting the squared values in-place
-        sort(nums.begin(), nums.end());
-        return nums;
+
+        // sort(nums.begin(), nums.end());
+        // return nums;
+
+ // usig two pointer approach
+        int left = 0;
+        int right = nums.size() - 1;
+        int pos = nums.size() - 1;
+        vector<int> result(nums.size());
+        while (left <= right) {
+            if (abs(nums[left]) < abs(nums[right])) {
+                result[pos] = nums[right] * nums[right];
+                right--;
+            } else {
+                result[pos] = nums[left] * nums[left];
+                left++;
+            }
+            pos--;
+        }
+        return result;
     }
 };
