26a7e73650c34d89e399e1c0e6ccf6373dc16183
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-20 07:31:35 +0500

    Time: 0 ms (100%), Space: 9.7 MB (51.63%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int goodpairs=0;
-         for( int i=0; i<nums.size(); i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                goodpairs++;
-                }
+        // Bruteforce Approach 
+        int n = nums.size();
+        int goodpair=0;
+        for(int i = 0; i < n; i++){
+            for( int j = i+1; j < n; j++){
+            if(nums[i] == nums[j])
+            goodpair++;
             }
-            
-         }
-         return goodpairs;
+        }
+        return goodpair;
+
     }
 };
-// optimize using hashmap
