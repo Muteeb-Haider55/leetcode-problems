019506cf6e1be1cb0c4c2851486128e76b360675
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-20 06:55:35 +0500

    Time: 0 ms (100%), Space: 14.9 MB (37.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,11 +2,25 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // Brute Force Approach
+        /*
         int n = nums.size();
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < n; j++){
                 if(nums[i] + nums[j] == target)
                 return {i, j};
+            }
+        }
+        return {};
+        */
+        // Optimized Approach
+        unordered_map<int, int> check;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (check.find(diff) != check.end()) {
+                return {i, check[diff]};
+            } else {
+                check.insert({nums[i], i});
             }
         }
         return {};
