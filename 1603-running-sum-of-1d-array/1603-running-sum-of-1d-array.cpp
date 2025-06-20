fa2420e76b9e48e383141277fec0c14782ffa2ea
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-20 15:14:45 +0500

    Time: 0 ms (100%), Space: 12.3 MB (97.82%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        // Optimized Aproach
-       
-       for (int i = 1; i < nums.size(); i++) {
-            nums[i] += nums[i - 1];
-        }
-        return nums;
-        
-        // Brute Force Approach
-       /* 
-        vector <int> res = nums;
-        int n = nums.size();
-        for(int i=1; i<n; i++){
-            res[i] += res[i-1];
-        }
-        return res;
-        */ 
+       for(int i =1 ; i <nums.size(); i++){
+       nums[i]+=nums[i-1];
+       }
+       return nums;
     }
 };
