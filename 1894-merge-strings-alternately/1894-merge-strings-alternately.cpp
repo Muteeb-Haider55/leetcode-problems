4b985637ad37b526328906f3754ed03aca1a8ba5
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-22 10:37:20 +0500

    Time: 4 ms (32.03%), Space: 8.3 MB (96.32%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i = 0, j = 0;
-        string word3 = "";
-        while (i < word1.size() || j < word2.size()) {
-            if (i < word1.size())
-                word3 += word1[i++];
-            if (j < word2.size())
-                word3 += word2[j++];
+        string word3;
+        int first = 0;
+        int second = 0;
+        int n1 = word1.size() ;
+        int n2 = word2.size() ;
+        while(first != n1 || second != n2){
+            if(first < n1){
+                word3.push_back(word1[first++]);
+            }
+            if(second < n2){
+                word3.push_back(word2[second++]);
+            }
         }
         return word3;
     }
