cf5929118771c6e05836150a51467f9c70a6ad20
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-25 16:57:03 +0500

    Time: 6 ms (8.08%) | Memory: 9.7 MB (93.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,16 +1,39 @@
+
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         string res;
+//         for (char c : s) {
+//             if (isalnum(c))
+//                 res += tolower(c);
+//         }
+//         int left = 0;
+//         int right = res.size() - 1;
+//         while (left < right) {
+//             if (res[left] != res[right])
+//                 return false;
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool isPalindrome(string s) {
         int left = 0;
         int right = s.size() - 1;
-        while (left <= right) {
-            if (!isalnum(s[left])) {
+        while (left < right) {
+            while (!isalnum(s[left]) && left < right) {
                 left++;
-                continue;
             }
-            if (!isalnum(s[right])) {
+            while(!isalnum(s[right]) && left < right) {
                 right--;
-                continue;
+               
             }
             if (tolower(s[left]) != tolower(s[right])) {
                 return false;
