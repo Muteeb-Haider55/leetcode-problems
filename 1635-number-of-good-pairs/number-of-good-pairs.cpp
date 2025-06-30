88e827961d63d0d7efb2aeaaf9636765d7b3fcfd
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-06-30 17:51:58 +0500

    Time: 0 ms (100.00%) | Memory: 9.9 MB (21.72%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        // Bruteforce Approach 
-        int n = nums.size();
-        int goodpair=0;
-        for(int i = 0; i < n; i++){
-            for( int j = i+1; j < n; j++){
-            if(nums[i] == nums[j])
-            goodpair++;
+        // Bruteforce Approach
+        /*
+            int n = nums.size();
+            int goodpair=0;
+            for(int i = 0; i < n; i++){
+                for( int j = i+1; j < n; j++){
+                if(nums[i] == nums[j])
+                goodpair++;
+                }
             }
+            return goodpair;
+            */
+            //Brute Force Approach
+        unordered_map<int, int> freq;
+        int count = 0;
+        for (int num : nums) {
+            if (freq.count(num)) {
+                count += freq[num];
+            }
+            freq[num]++;
         }
-        return goodpair;
 
+        return count;
     }
 };
