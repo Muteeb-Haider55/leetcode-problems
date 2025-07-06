34cdfc4850756f0008beef260e723fee65ee9feb
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-07-06 14:00:18 +0500

    Time: 0 ms (100%), Space: 8.5 MB (37.02%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        string res;
+        int right = -1;
+        for (int i = 0; i < word.size(); i++) {
+            if (word[i] == ch) {
+                right = i;
+                break;
+            }
+        }
+        if (right == -1)
+            return word;
+        int k = right + 1;
+        for (int j = right; j >= 0; j--) {
+            res.push_back(word[j]);
+        }
+        for (int i = k; i < word.size(); i++) {
+            res.push_back(word[i]);
+        }
+        if (res.empty())
+            return word;
+        return res;
+    }
+};
