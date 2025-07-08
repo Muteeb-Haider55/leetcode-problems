a667e7f3a4c0b9bc98d208237c37a11e224ec13e
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-07-08 08:06:46 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (37.10%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        string res;
+    int left =0;
+    int right = 0;
+
+        for(int i =0;i<word.size();i++){
+            if(word[i]==ch){
+                right = i;
+                break;
+            }
+        }
+        while(left<right){
+            swap(word[left], word[right]);
+            left++;
+            right--;
+        }
+        return word;
+        /*string res;
         int right = -1;
         for (int i = 0; i < word.size(); i++) {
             if (word[i] == ch) {
@@ -21,5 +36,6 @@ public:
         if (res.empty())
             return word;
         return res;
+        */
     }
 };
