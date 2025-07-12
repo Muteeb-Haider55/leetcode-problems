5e078bd3a1a84b34fb3468b69cf73e77a81b9ae4
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-07-12 09:25:39 +0500

    Time: 0 ms (100%), Space: 8.8 MB (60.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -31,3 +31,25 @@ public:
 
     }
 };
+
+
+//Optimize and clean code 
+/*class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+                if (c == ')' && st.top() != '(') return false;
+                if (c == '}' && st.top() != '{') return false;
+                if (c == ']' && st.top() != '[') return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+*/
