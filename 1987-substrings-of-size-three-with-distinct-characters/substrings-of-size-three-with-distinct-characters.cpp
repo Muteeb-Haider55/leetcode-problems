06a288a117ad887429657a910e108d207eb22522
Mateeb Haider <b-27240@student.usa.edu.pk>
2025-07-30 18:40:24 +0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (89.14%) - LeetSync

---
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        if(s.size()<3) return 0;
+        int goodStringCount = 0;
+        for(int i = 0; i < s.size() - 2; i++){//why n-2 to prevent out of bound
+        if(s[i] != s[i+1] && s[i] != s[i+2] && s[i+1] != s[i+2]){
+          goodStringCount++;
+        }
+        }
+  return goodStringCount;
+    }
+};
